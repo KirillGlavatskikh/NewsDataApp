0311108763dcd0b652fdc4c162433172d368ae88
KirillGlavatskikh <k.glavatskikh@mail.ru>
2023-02-06 16:35:31 +0400

    Initial commit

---
+++ src/main/java/com/example/newsdataapp/NewsDataAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.newsdataapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsDataAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsDataAppApplication.class, args);
+    }
+
+}
